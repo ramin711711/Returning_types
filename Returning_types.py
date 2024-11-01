75774bf10e6f0fc5f41b5a1d848375315d594929
ramin711711 <raminhuseynov@hotmail.com>
2024-11-01 13:12:36 +0400

    Add files via upload

---
+++ Returning_types.py
@@ -0,0 +1,31 @@
+# 1.    # def newfunc(data: list[str]) -> dict[str, int]:
+        #     result = {}
+        #     for i in data:
+        #         result[i] = len(i) 
+        #     print(result)
+        # newfunc(('h', 'm', 'hi', '3444636363464746756889759'))
+
+# 2.    # def _(a: int, b: str):
+        #     nums = str(a) + b
+        #     return f'Final number is: {nums}'
+        # print(_(100,'A+'))
+
+# 3.    # def _(a: int, b: int) -> int:
+        #     nums = a + b
+        #     return f'Final number is: {nums}'
+        # print(_(100, 10))
+
+# 4.    # def _(a: int, b: str):
+        #     nums = str(a) + b
+        #     return f'Final number is: {nums}'
+        # print(_(100,'A+'))
+
+# 5.    # def _(a: str, b: str) -> str:
+        #     nums = a + b
+        #     return f'Final number is: {nums}'
+        # print(_('95 | ','A+'))
+
+# 6.    # def _(a: float, b: float) -> bool:
+        #     nums = a + b
+        #     return f'Final number is: {nums}'
+        # print(_(2.1, 0.5))
